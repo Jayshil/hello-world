266bbb878ed914ae339f30c702731563fee41e7b
Jayshil Patel <40012574+Jayshil@users.noreply.github.com>
2019-12-03 12:38:52 +0530

    Update p1.py
    Added y

--- p1.py
+++ p1.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 print('This is my first Github repository')
 
 x = np.random.normal(0,0.5,10000)
+y = np.random.normal(1,0.2,10000)
 
 plt.hist(x,bins=500)
+plt.hist(y,bins=500)
 plt.show()
