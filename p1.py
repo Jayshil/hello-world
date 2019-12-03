03b2e71a09ae24b1782e028f9103008481f6bf4c
Jayshil <jayshilpatel147@gmail.com>
2019-12-03 12:57:14 +0530

    Entered a new line

--- p1.py
+++ p1.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 print('This is my first Github repository')
+print('This is my first branching tutorial...')
 
 x = np.random.normal(0,0.5,10000)
 y = np.random.normal(1,0.2,10000)
